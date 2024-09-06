ffba3950a2d378f20266516bc003b7c0eaef4680
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2024-09-06 21:08:53 +0200

    Remove clone defs

--- src/world/entities.h
+++ src/world/entities.h
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 void destroyEntities(void);
-void resetClones(void);
 void resetEntities(void);
 void activeEntities(char *targetName, int active);
 void drawEntities(int background);
