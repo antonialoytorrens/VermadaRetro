1c35af573595068e5b7eefb81f8c9f3dc52eb954
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2024-10-01 12:57:04 +0200

    Add emscripten build info

--- common.mk
+++ common.mk
@@ -15,11 +15,22 @@ GAME_OBJS = $(OBJS) $(OUT)/src/main.o
 
 MAP_OBJS = $(OBJS) $(OUT)/src/mapEditor.o
 
-all: $(PROG) $(MAP_PROG)
+all: $(PROG) $(MAP_PROG) credits
+
+credits: data/misc/credits.txt
+
+data/misc/credits.txt: raw/credits.txt
+	@if echo '$(CC)' | grep -q 'emcc'; then \
+		echo "Generating credits with Emscripten build info..."; \
+		sed 's/.*%%EMSCRIPTEN_CREDITS%%.*/1 Built with Emscripten $(EMVERSION)/' $< > $@; \
+	else \
+		echo "Generating credits..."; \
+		sed '/%%EMSCRIPTEN_CREDITS%%/d' $< > $@; \
+	fi
 
 $(OUT)/%.o: %.c %.h $(DEPS)
 	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
 
 clean:
-	$(RM) -rf $(OUT) $(PROG) $(PROG).wasm $(MAP_PROG) $(MAP_PROG).wasm index.html index.js $(LOCALE_MO)
+	$(RM) -rf $(OUT) $(PROG) $(PROG).wasm $(MAP_PROG) index.html index.js data/misc/credits.txt $(LOCALE_MO)
